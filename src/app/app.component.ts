cf2d6bd265a15f8740300c94555c6eae2d44c243
Mbote-Joseph <mbotejoseph001@gmail.com>
2020-08-06 13:33:42 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Test';
+  title = 'Drag & Drop in Angular App';
+  website = 'https://joseph-mbote.netlify.com';
+
+  todos = [
+    {
+      name: 'Angular',
+      category: 'Web Development'
+    },
+    {
+      name: 'Flexbox',
+      category: 'Web Development'
+    },
+    {
+      name: 'iOS',
+      category: 'App Development'
+    },
+    {
+      name: 'Java',
+      category: 'Software development'
+    }
+  ];
+
+  completed = [
+    {
+      name: 'Android',
+      category: 'Mobile Development'
+    },
+    {
+      name: 'MongoDB',
+      category: 'Databases'
+    },
+    {
+      name: 'ARKit',
+      category: 'Augmented Reality'
+    },
+    {
+      name: 'React',
+      category: 'Web Development'
+    }
+  ];
+
+  onDrop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex, event.currentIndex);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MatCardModule} from '@angular/material/card';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
